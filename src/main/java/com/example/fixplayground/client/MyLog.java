37d6a63eccb98429f290ce765f5ff8730cf134c6
Anup Arakkal <anup.anup2001@gmail.com>
2020-12-22 23:52:27 -0500

    Added STOMP Support to push session data to client

--- src/main/java/com/example/fixplayground/client/MyLog.java
+++ src/main/java/com/example/fixplayground/client/MyLog.java
@@ -1,14 +1,20 @@
 package com.example.fixplayground.client;
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import quickfix.Log;
 import quickfix.LogFactory;
 import quickfix.SessionID;
 
 public class MyLog implements LogFactory, Log {
 
+    private SimpMessagingTemplate template;
+    public MyLog(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
     @Override
     public Log create(SessionID sessionID) {
-        return new MyLog();
+        return new MyLog(template);
     }
 
     @Override
@@ -19,12 +25,13 @@ public class MyLog implements LogFactory, Log {
     @Override
     public void onIncoming(String s) {
         System.out.println("Yay - incoming: " + s);
+        this.template.convertAndSend("/topic/data", s);
     }
 
     @Override
     public void onOutgoing(String s) {
         System.out.println("Yay - outgoing: " + s);
-
+        this.template.convertAndSend("/topic/data", s);
     }
 
     @Override
--- src/main/java/com/example/fixplayground/client/SessionManagerService.java
+++ src/main/java/com/example/fixplayground/client/SessionManagerService.java
@@ -1,13 +1,16 @@
 package com.example.fixplayground.client;
 
 
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 class SessionManagerService {
     private SessionManager sessionManager;
-    SessionManagerService() {
-        this.sessionManager = new SessionManager();
+    private SimpMessagingTemplate template;
+    SessionManagerService(SimpMessagingTemplate template) {
+        this.template = template;
+        this.sessionManager = new SessionManager(template);
     }
 
     public void connect() {
